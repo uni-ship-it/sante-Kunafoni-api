0b234690b136643a08fcb0c1446e674b241dfd4b
Famory_Josue_Sissoko <famoryjosuesissoko@gmail.com>
2026-06-18 14:32:37 +0000

    Initialisation propre du dépôt

---
+++ README.md
@@ -0,0 +1,6 @@
+# Santé Kunafoni API
+
+API REST de gestion des patients, médecins,
+consultations et rendez-vous.
+
+Projet réalisé dans le cadre du TP UML et API REST.
---
+++ src/test/java/com/kounafoni/api/santekounafoni/SantekounafoniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kounafoni.api.santekounafoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantekounafoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
